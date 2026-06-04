0a8dc0f2f4a6e09ef58f5547f6c4660a62bc3b28
mishanull314 <mishanull512@gmail.com>
2026-06-04 18:42:16 +0300

    feat: Added marked list as example

--- markdown-2-gost-word/markdown-2-gost-word.cpp
+++ markdown-2-gost-word/markdown-2-gost-word.cpp
@@ -1,6 +1,30 @@
 ﻿#include <iostream>
+#include <string>
+#include <fstream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::ifstream readFile("Start.md");
+
+    if (readFile.is_open())
+    {
+        std::string data;
+
+        std::getline(readFile, data);
+
+
+        // 1. Very bad, not this example. Check title
+        if (data.find("- ") != std::string::npos)
+        {
+            std::cout << "This marked list" << std::endl;
+        }
+        else
+        {
+            std::cout << "This marked list" << std::endl;
+        }
+    }
+    else
+    {
+        std::cout << "File not opened!";
+    }
 }
