142b5ca7945deb3c0375af40a44eb681cfead6df
manglefox <mangle87fox@gmail.com>
2026-06-04 17:24:02 +0300

    update: add new version: code initialization

--- markdown-2-gost-word/markdown-2-gost-word.cpp
+++ markdown-2-gost-word/markdown-2-gost-word.cpp
@@ -27,4 +27,34 @@ int main()
     {
         std::cout << "File not opened!";
     }
+   ifstream file("input.txt");
+    if (!file.is_open()) {
+        cout << "ne udalos' otkrit' fail\n";
+        return 1;
+    }
+    string line;
+    int lineNumber = 1;
+    while (getline(file, line)) {
+        bool isCode = false;//Проверили всё, что хотели, и только один раз в конце принимаем решение
+        if (line.find('{') != string::npos || line.find('}') != std::string::npos)
+            isCode = true;
+        if (line.find(';') != string::npos)
+            isCode = true;
+        if (line.find("int") != string::npos ||
+            line.find("if") != string::npos ||
+            line.find("else") != string::npos ||
+            line.find("for") != string::npos ||
+            line.find("while") != string::npos ||
+            line.find("return") != string::npos)
+            isCode = true;
+        cout << "stroka " << lineNumber << ": ";
+        if (isCode)
+            cout << "kod - " << line << std::endl;
+        else
+            cout << "tekst - " << line << std::endl;
+        lineNumber++;
+    }
+    file.close();
+    return 0;
+}
 }
