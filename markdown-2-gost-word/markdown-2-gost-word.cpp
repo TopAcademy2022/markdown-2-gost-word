ce1f83724a3cd1a17e6e7ee892b8fd51c1ab45b7
mishanull314 <mishanull512@gmail.com>
2026-06-09 16:29:26 +0300

    Feat: Add IsMarkedList function

--- markdown-2-gost-word/markdown-2-gost-word.cpp
+++ markdown-2-gost-word/markdown-2-gost-word.cpp
@@ -2,6 +2,12 @@
 #include <fstream>
 #include <string>
 
+bool IsMarkedList(std::string line) {
+    bool res = false;
+    if (line.find("- ") != std::string::npos) res = true;
+    return res;
+}
+
 int main()
 {
     std::ifstream readFile("start.md");
@@ -13,8 +19,7 @@ int main()
         std::getline(readFile, data);
 
 
-	    // 1. Very bad, not this example. Check title
-        if (data.find("- ") != std::string::npos)
+        if (IsMarkedList(data))
         {
             std::cout << "This marked list" << std::endl;
         }
