befea643d7565112c9e0175318e7e6e74abcd800
VopeI <28.kosenkov.1985@mail.ru>
2026-07-07 20:39:17 +0300

    feat: add realization MdSectionConverter::SaveToGostWord

--- markdown-2-gost-word/MdSectionConverter.h
+++ markdown-2-gost-word/MdSectionConverter.h
@@ -22,6 +22,6 @@ public:
 	MdSectionConverter(std::list<MdSection*> sections);
 	~MdSectionConverter();
 
-	XmlServiceStatus SaveToGostWord();
+	XmlServiceStatus SaveToGostWord(const std::wstring& outputPath);
 };
 
