0b5381fd2fbbc8268c1f4189692e1965aa75bb67
manglefox <mangle87fox@gmail.com>
2026-07-07 21:34:02 +0300

    Add realization MdSectionParser::ParseText

--- markdown-2-gost-word/MdSectionParser.cpp
+++ markdown-2-gost-word/MdSectionParser.cpp
@@ -1,11 +1,13 @@
 #include "MdSectionParser.h"
-
+#include <sstream> 
+#include <string>
+#include <list>
 std::list<MdSection*> MdSectionParser::ParseText(std::string text)
 {
-	std::list<MdSection*> result;
+    std::list<MdSection*> result = std::list<MdSection*>();
     MdSectionRule mdRules;
 
-    if (!text.empty())
+   if (!text.empty())
     {
         const MdSectionType* section = mdRules.GetTypeFromStartRules(text);
         if (section != nullptr)
@@ -13,6 +15,34 @@ std::list<MdSection*> MdSectionParser::ParseText(std::string text)
             result.push_back(new MdSection(text, *section));
         }
     }
+    //return result;//конец первоначаль шаблона, нужно ли???????? что делать потом с гитом 
+        std::istringstream stream(text);
+        std::string line;
+        std::string segment;
 
-    return result;
-}
+        while (std::getline(stream, line))
+        {
+            if (line.empty())
+            {
+                if (!segment.empty())
+                {
+                    auto sectionType = mdRules.GetTypeFromStartRules(segment);// определяем тип блока и создаём секцию
+                    if (sectionType)
+                        result.push_back(new MdSection(segment, *sectionType));
+                    segment.clear();
+                }
+            }
+            else
+            {
+                segment += line + "\n";// не пустая сторока = сегмент 
+            }
+        }
+        if (!segment.empty()) //последний блок без пустой строки в конце
+        {
+            auto sectionType = mdRules.GetTypeFromStartRules(segment);
+            if (sectionType)
+                result.push_back(new MdSection(segment, *sectionType));
+        }
+
+        return result;
+    }
